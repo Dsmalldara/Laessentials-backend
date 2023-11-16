d70d29f095061765667e6056f0e16fd490d1d7e4
Dsmalldara <100885837+Dsmalldara@users.noreply.github.com>
2023-11-16 13:08:41 +0100

    secondcommit

--- server.js
+++ server.js
@@ -3,29 +3,36 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // Use process.env.PORT if available, otherwise default to 5000
 const Stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.get('/',(req,res)=>{
-    res.send('Hello WorLd');
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
-app.post('/payment',(req,res)=>{
-    Stripe.charges.create({
-        amount:req.body.amount,
-        currency:'usd',
-        source:req.body.token.id,
-        description:"Essential's payment"
-    },(err,charge)=>{
-        if(err){
-            console.log(err);
-            res.status(500).send(err);
-        }else{
-            res.status(200).send(charge);
-        }
-    })
-})
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+
+app.post('/payment', (req, res) => {
+  Stripe.charges.create(
+    {
+      amount: req.body.amount,
+      currency: 'usd',
+      source: req.body.token.id,
+      description: "Essential's payment",
+    },
+    (err, charge) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(charge);
+      }
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
